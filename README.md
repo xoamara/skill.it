25b1c658fce2d3993b37e5f90a73c60a356aeb9a
Truc Ly Le <itstruclyle@gmail.com>
2019-02-03 20:05:34 -0800

    Define data routes (#19)
    * Define data routes for skill.it
    
    * Remove spread operator
    
    * Add semicolons

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # skill.it
+
+## Quick Start
+
+1. Clone the repository
+
+```
+git clone git@github.com:macdhollister/skill.it.git
+cd skill.it
+```
+
+2. Install dependencies
+
+```
+npm install
+```
+
+3. Run `schema.sql`
+
+4. Edit `knexfile.js` with database configs
+
+5. Run migration and seeding
+
+```
+npm install knex -g
+knex migrate:latest
+knex seed:run
+```
+
+6. Launch skill.it
+
+```
+node server
+```
--- server.js
+++ server.js
@@ -1,43 +1,18 @@
-require("dotenv").config();
-const express = require("express");
 const ejs = require("ejs");
 
-
-const db = require("./models");
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
-// EJS
-
 app.set("view engine", "ejs");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+const dataRoutes = require("./routes/dataRoutes");
+app.use(dataRoutes);
 
-const syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(() => {
-    app.listen(PORT, () => {
-        console.log(
-            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-            PORT,
-            PORT
-        );
-    });
+app.listen(PORT, function() {
+    console.log(`Server listening on PORT ${PORT}`);
 });
-
-module.exports = app;
