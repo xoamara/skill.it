40a0372f4bf9b1debe99905484212c73b960c7c8
macdh <macdhollister@gmail.com>
2019-02-06 20:33:58 -0800

    test route added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const session = require("express-session");
+const passport = require("./config/passport");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -14,10 +15,14 @@ app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Test route -- delete this later
-app.get("/", (req, res) => {
-    res.send("Welcome to Passport with Sequelize and no Handlebars!");
-});
+// app.get("/", (req, res) => {
+//     res.send("Welcome to Passport with Sequelize and no Handlebars!");
+// });
 
 // Routes
 const authRoutes = require("./routes/authRoutes");
