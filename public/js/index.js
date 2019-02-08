3a23210736bf28fef56cd73c3a1bc56633c8d41e
Truc Ly Le <itstruclyle@gmail.com>
2019-02-07 21:08:20 -0800

    Render users

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,80 @@
 /* eslint-disable prefer-arrow-callback */
 $(function () {
 
+    function showUsers() {
+        $.ajax({
+            method: "GET",
+            url: "/api/users"
+        }).then((res) => {
+            console.log(res);
+
+            const tmplt = res.map(user => {
+                let str = `
+                <div class="box is-shadow">
+                    <article class="media">
+                        <div class="media-left">
+                            <figure class="image is-64x64">
+                                <img class="is-rounded" src="https://bulma.io/images/placeholders/64x64.png">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <div class="content userHeader">
+                                <p>
+                                    <strong class="userName" id="${user.id}">
+                                        ${user.username}</strong>
+                                    <medium>
+                                        <a>
+                                            <span class="icon is-small">
+                                                <i class="fas fa-envelope emailContact"></i>
+                                            </span>
+                                        </a>
+                                    </medium>
+                                    <br>
+                                    ${user.description}
+                                    <br>
+                                    <br>
+                                    <br>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+
+                    <!-- Teaching/Learning -->
+                    <div class="teaching">
+                        <h6 class="is-size-6">Teaching</h6>
+                        <div class="buttons are-small">
+                    `;
+                user.SkillToLearns.forEach(skill => {
+                    str += `<a class="button">${skill.skillId}</a>`;
+                });
+
+                str += `
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="learning">
+                        <h6 class="is-size-6">Learning:</h6>
+                        <div class="buttons are-small">
+                `;
+                user.SkillToTeaches.forEach(skill => {
+                    str += `<a class="button">${skill.skillId}</a>`;
+                });
+                str += `
+                        </div>
+                    </div>
+                </div>
+                `;
+
+                return str;
+            }).join("");
+
+            $("#users").append($(tmplt));
+        });
+    }
+
+    showUsers();
+
     $(".searchDiv").on("click", ".button", function (event) {
         event.preventDefault();
 
