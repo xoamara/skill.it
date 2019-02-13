38408d148cfc84ad82fcb28f9cfcd8b9f04258dd
macdh <macdhollister@gmail.com>
2019-02-13 13:28:46 -0800

    temporary fix for LINT issue in server.js

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use(flash());
 
 // Global Vars
 app.use((req, res, next) => {
-    res.locals.success_msg = req.flash("success_msg");
-    res.locals.error_msg = req.flash("error_msg");
+    // res.locals.success_msg = req.flash("success_msg"); // COMMENTED OUT FOR LINT
+    // res.locals.error_msg = req.flash("error_msg");
     res.locals.error = req.flash("error");
     next();
 });
