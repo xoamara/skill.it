277d58422c6f13967c325efa841a63433c3b4ea4
macdh <macdhollister@gmail.com>
2019-02-13 01:27:33 -0800

    updated passport.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const sequelize = require("sequelize");
+// const sequelize = require("sequelize");
 const flash = require("connect-flash");
 const session = require("express-session");
 const passport = require("./config/passport");
