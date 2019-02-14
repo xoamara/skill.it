9091de380487b9f34a88117d181fcda6fd59aad8
macdh <macdhollister@gmail.com>
2019-02-14 04:31:38 -0800

    Refactored home page to render with EJS rather than AJAX (INSANE PERFORMANCE GAINS)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,51 @@
 const path = require("path");
 const router = require("express").Router();
-const { isAuthenticated } = require("../config/middleware/isAuthenticated");
+// const { isAuthenticated } = require("../config/middleware/isAuthenticated");
+
+const db = require("../models");
 
 const dir = function(ejsFileName) {
     return path.join(__dirname, `../public/views/pages/${ejsFileName}`);
 };
 
-// Home page
+// Home Page
 router.get("/", (req, res) => {
-    res.render(dir("index.ejs"));
+    db.User.findAll({
+        attributes: ["id", "username", "email", "description", "createdAt", "updatedAt"],
+        include: [
+            {
+                model: db.SkillToLearn,
+                as: "skillsLearning",
+                attributes: ["skillId", "createdAt", "updatedAt"]
+            },
+            {
+                model: db.SkillToTeach,
+                as: "skillsTeaching",
+                attributes: ["skillId", "createdAt", "updatedAt"]
+            }
+        ]
+    }).then(userResults => {
+        db.Skill.findAll({
+            include: [
+                {
+                    model: db.SkillToLearn,
+                    as: "usersLearning",
+                    attributes: ["userId", "createdAt", "updatedAt"]
+                },
+                {
+                    model: db.SkillToTeach,
+                    as: "usersTeaching",
+                    attributes: ["userId", "createdAt", "updatedAt"]
+                }
+            ]
+        }).then(skillResults => {
+            console.log(skillResults[0]);
+            res.render(dir("index.ejs"), {
+                users: userResults,
+                skills: skillResults
+            });
+        });
+    });
 });
 
 // Send login page
@@ -40,11 +77,6 @@ router.get("/email", (req, res) => {
 // About skill.it
 router.get("/about", (req, res) => {
     res.render(dir("about.ejs"));
-});
-
-// Test route for ensuring Authentication
-router.get("/testAuthed", isAuthenticated, (req, res) => {
-    res.render(dir("test.ejs"));
 });
 
 // Welcomes the user and tells them if they are logged in
